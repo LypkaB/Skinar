efd26b4900cb3b6eb1e0bcf97fc002ba7a377d56
LypkaB <lpk.bdn@gmail.com>
2019-07-31 12:34:26 +0300

    Added «Gulp» in project

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+var concatCSS   = require('gulp-concat-css');
+
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass'], function() {
+
+    browserSync.init({
+        server: "/Users/l.b./HTML\ \&\ CSS/1.\ Training/Skynar.com/"
+    });
+
+    gulp.watch("/Users/l.b./HTML\ \&\ CSS/1.\ Training/Skynar.com/sass/*.sass", ['sass']);
+    gulp.watch("/Users/l.b./HTML\ \&\ CSS/1.\ Training/Skynar.com/*.html").on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+    return gulp.src("/Users/l.b./HTML\ \&\ CSS/1.\ Training/Skynar.com/sass/*.sass")
+        .pipe(sass())
+        .pipe(concatCSS('style.css'))
+        .pipe(gulp.dest("/Users/l.b./HTML\ \&\ CSS/1.\ Training/Skynar.com/css"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
